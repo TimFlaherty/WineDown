38668a610481a9fcbe0669f3f0d1249507a0fc79
Tim Flaherty <TMF@Tim-Flahertys-MacBook-Pro.local>
2017-11-11 12:42:16 -0800

    Add views to simplify queries

--- dbsetup/db_create.sql
+++ dbsetup/db_create.sql
@@ -9,8 +9,8 @@ USE winedown;
 CREATE TABLE winery (
 wineryid INT(6) ZEROFILL NOT NULL AUTO_INCREMENT PRIMARY KEY,
 wineryname VARCHAR(40) NOT NULL,
-lat DECIMAL(10, 8),
-lng DECIMAL(11, 8),
+lat DECIMAL(10, 8) NOT NULL,
+lng DECIMAL(11, 8) NOT NULL,
 address VARCHAR(256) NOT NULL,
 hours VARCHAR(256),
 url VARCHAR(2083),
@@ -35,7 +35,7 @@ email VARCHAR(256)
 
 CREATE TABLE review (
 reviewid INT(9) ZEROFILL NOT NULL AUTO_INCREMENT PRIMARY KEY,
-wineid INT(7) ZEROFILL NOT NULL,
+wineid INT(7) ZEROFILL,
 wineryid INT(6) ZEROFILL NOT NULL,
 uid INT(8) ZEROFILL NOT NULL,
 rating INT(1) NOT NULL,
@@ -44,3 +44,36 @@ FOREIGN KEY (wineid) REFERENCES wine(wineid),
 FOREIGN KEY (wineryid) REFERENCES winery(wineryid),
 FOREIGN KEY (uid) REFERENCES usr(uid)
 );
+
+CREATE VIEW winerating AS
+	SELECT a.wineid,
+	a.wineryid,	
+	b.varietal,
+	AVG(a.rating) AS winerating
+	FROM review a
+	JOIN wine b 
+	ON a.wineid = b.wineid
+	WHERE a.wineid > 0 
+	GROUP BY wineid;
+	
+CREATE VIEW wineryrating AS
+	SELECT wineryid, 
+	AVG(rating) AS wineryrating 
+	FROM review 
+	WHERE wineid IS NULL 
+	GROUP BY wineryid;
+
+CREATE VIEW winerypins AS
+	SELECT a.wineryid, 
+	a.wineryname, 
+	a.lat, 
+	a.lng, 
+	a.hours, 
+	GROUP_CONCAT(DISTINCT b.varietal SEPARATOR ", ") AS varietals,
+	c.wineryrating
+	FROM winery a 
+	LEFT JOIN winerating b 
+	ON a.wineryid = b.wineryid 
+	LEFT JOIN wineryrating c
+	ON a.wineryid = c.wineryid
+	GROUP BY a.wineryid;
